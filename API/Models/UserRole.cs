149867a77f369ae67355c829ccd6643c39e32c10
fhmanwar <fhmianwar@gmail.com>
2020-09-14 14:01:16 +0700

    Initial Code First

---
+++ API/Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    [Table("TB_M_UserRole")]
+    public class UserRole
+    {
+    }
+}
